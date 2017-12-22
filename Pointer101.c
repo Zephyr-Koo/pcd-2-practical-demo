f706dc707b2ffa98b0dc8e1871cf51daa08ac1c0
Zephyr Koo <30658485+Zephyr-Koo@users.noreply.github.com>
2017-12-22 21:29:55 +0800

    Added message output in swap function

--- Pointer101.c
+++ Pointer101.c
@@ -23,51 +23,56 @@ void demoPointer()
 
     /* NOTE:
        (1) numAddr stores the address of num
-       (2) *numAddr refers to the reference (pointer) which store at its address
+       (2) *numAddr refers to the reference (pointer) which store at its
+           corresponding address
     */
 
     printf("Value %d was stored in address %d. \n", num, numAddr);
-    printf("The address %d contains the value %d. \n", numAddr, *numAddr);
+    printf("The address %d contains the value %d. \n\n", numAddr, *numAddr);
 }
 
 void demoSwap1()
 {
     int num1 = 11, num2 = 22;
 
-    printf("Before (1) : num1 = %d | num2 = %d\n", num1, num2);
+    printf("Before  (1) : num1 = %d | num2 = %d\n", num1, num2);
 
     swap1(num1, num2);
 
-    printf("After  (1) : num1 = %d | num2 = %d\n\n", num1, num2);
+    printf("After   (1) : num1 = %d | num2 = %d\n\n", num1, num2);
 }
 
 void demoSwap2()
 {
     int num1 = 33, num2 = 44;
 
-    printf("Before (2) : num1 = %d | num2 = %d\n", num1, num2);
+    printf("Before  (2) : num1 = %d | num2 = %d\n", num1, num2);
 
     swap2(&num1, &num2);
 
-    printf("After  (2) : num1 = %d | num2 = %d\n\n", num1, num2);
+    printf("After   (2) : num1 = %d | num2 = %d\n\n", num1, num2);
 }
 
 /* Carry out swapping but only applies within the function scope ONLY */
-void swap1(int x, int y)
+void swap1(int num1, int num2)
 {
     int temp;
 
-    temp = x;
-    x    = y;
-    y    = temp;
+    temp = num1;
+    num1 = num2;
+    num2 = temp;
+
+    printf("Swapped (1) : num1 = %d | num2 = %d\n", num1, num2);
 }
 
 /* Carry out swapping with reference to the corresponding address */
-void swap2(int *x, int *y)
+void swap2(int *num1, int *num2)
 {
     int temp;
 
-    temp = *x;
-    *x   = *y;
-    *y   = temp;
+    temp  = *num1;
+    *num1 = *num2;
+    *num2 = temp;
+
+    printf("Swapped (2) : num1 = %d | num2 = %d\n", *num1, *num2);
 }
