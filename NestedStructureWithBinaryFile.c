482d865998d42196c156c560b364e1aaab00dcc6
Zephyr Koo <30658485+Zephyr-Koo@users.noreply.github.com>
2018-12-24 23:46:18 +0800

    Create NestedStructureWithBinaryFile.c

---
+++ NestedStructureWithBinaryFile.c
@@ -0,0 +1,93 @@
+#define FRIEND_SIZE 5
+#define BINARY_FILE "StaffRecords.dat"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#pragma warning(disable: 4996)
+
+void saveFriendList();
+void displayFriendList();
+
+typedef struct {
+    int day;
+    int month;
+    int year;
+} Date;
+
+typedef struct {
+    char name[50];
+    Date dateOfBirth;
+    char gender;
+} Friend;
+
+// Predefined records
+Friend friendList[FRIEND_SIZE] =
+{
+    { "Koo", 25, 10, 1991, 'M' },
+    { "Ng" ,  3,  3, 1991, 'F' },
+    { "Lim", 29,  2, 2000, 'M' },
+    { "Chan", 1,  1, 1999, 'F' },
+    { "Tan", 11, 12, 1993, 'M' }
+};
+
+int main()
+{
+    saveFriendList();
+
+    puts("");
+    displayFriendList();
+
+    return 0;
+}
+
+
+void saveFriendList()
+{
+    FILE *fOutput = fopen(BINARY_FILE, "wb"); // write to binary file
+
+    if (fOutput != NULL)
+    {
+        for (int i = 0; i < FRIEND_SIZE; i++)
+        {
+            fwrite(&friendList[i], sizeof(friendList[i]), 1, fOutput); // save one record at a time
+        }
+
+        printf("%s created successfully! \n", BINARY_FILE);
+        fclose(fOutput);
+    }
+}
+
+
+void displayFriendList()
+{
+    FILE *fInput = fopen(BINARY_FILE, "rb"); // read from file content
+
+    if (fInput != NULL)
+    {
+        Friend friend;
+
+        printf("Reading %s... \n\n", BINARY_FILE);
+
+        do
+        {
+            fread(&friend, sizeof(friend), 1, fInput); // read one record at a time
+
+            if (!feof(fInput)) // have we reached end-of-file?
+            {
+                printf("Name   : %s \n", friend.name);
+
+                printf("DOB    : %d/%d/%d \n",
+                    friend.dateOfBirth.day,
+                    friend.dateOfBirth.month,
+                    friend.dateOfBirth.year);
+
+                printf("Gender : %c \n", friend.gender);
+
+                puts("");
+            }
+        } while (!feof(fInput)); // continue reading until reaching end-of-file
+
+        fclose(fInput);
+    }
+}
